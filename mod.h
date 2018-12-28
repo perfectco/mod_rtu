7ae91a7bd666a7c9a8f6928b900341ab21d15eb3
Philip Odom <podom.pi@gmail.com>
2018-12-28 14:57:27 -0800

    add crc checking for tx

--- mod.h
+++ mod.h
@@ -1,13 +1,20 @@
 #ifndef __MOD_H__
 #define __MOD_H__
 
-#define MOD_RTU_MAX_MSG_LENGTH 256
-
 #include <inttypes.h>
 
+#define MOD_RTU_ADDR_OFFSET 0
+#define MOD_RTU_FUNCTION_OFFSET 1
+#define MOD_RTU_DATA_OFFSET 2
+#define MOD_RTU_CRC_LENGTH 2
+#define MOD_RTU_MAX_DATA_LENGTH 252
+
+
 typedef struct mod_rtu_msg_s {
-  uint16_t length;
-  uint8_t buf[MOD_RTU_MAX_MSG_LENGTH];
+  uint8_t address;
+  uint8_t function;
+  uint8_t data_length;
+  uint8_t data[MOD_RTU_MAX_DATA_LENGTH];
 } mod_rtu_msg_t;
 
 typedef enum mod_rtu_error_e {
