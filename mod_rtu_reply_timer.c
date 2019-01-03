1b78b3e969b65295fe5013f8a36c26599dedd1c2
Philip Odom <podom.pi@gmail.com>
2019-01-03 15:00:13 -0800

    fix reply_timer app_timer context

--- mod_rtu_reply_timer.c
+++ mod_rtu_reply_timer.c
@@ -1,11 +1,19 @@
 #include "mod_rtu_reply_timer.h"
 
+#define NRF_LOG_MODULE_NAME mod_rtu_reply_timer
+#define NRF_LOG_LEVEL 4
+#include "nrf_log.h"
+NRF_LOG_MODULE_REGISTER();
+
 static void app_timer_callback(void *context) {
+  NRF_LOG_DEBUG("timer callback");
   if (!context) {
     return;
   }
+  NRF_LOG_DEBUG("context OK");
   mod_rtu_reply_timer_t * me = (mod_rtu_reply_timer_t *)context;
   if (me->callback) {
+    NRF_LOG_DEBUG("calling callback");
     me->callback(me->callback_context);
   }
 }
@@ -22,7 +30,7 @@ mod_rtu_error_t mod_rtu_reply_timer_init(mod_rtu_reply_timer_t *const me, const 
 
 mod_rtu_error_t mod_rtu_reply_timer_start(mod_rtu_reply_timer_t *const me) {
   mod_rtu_reply_timer_stop(me);
-  app_timer_start(&me->timer, APP_TIMER_TICKS(me->timeout), me->callback_context);
+  app_timer_start(&me->timer, APP_TIMER_TICKS(me->timeout), me);
   return mod_rtu_error_ok;
 }
 
